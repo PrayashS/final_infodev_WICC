594cd9bbee20c0f7b1ae57d7935fd7559811a7b1
Prayash Shrestha <sthaprayash09@gmail.com>
2022-03-19 02:13:40 +0545

    Admin dashboard with user data shown and delete function implemented

--- src/app/dashboard/admin-dashboard/admin-dashboard.component.ts
+++ src/app/dashboard/admin-dashboard/admin-dashboard.component.ts
@@ -1,4 +1,8 @@
+import { Input } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { AdminService } from 'src/app/products/service/admin.service';
+import { dataModel } from './admin.model';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -6,10 +10,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./admin-dashboard.component.css']
 })
 export class AdminDashboardComponent implements OnInit {
-
-  constructor() { }
+  dataModelObj : dataModel = new dataModel()
+  userData !: any;
+  constructor(private api: AdminService) { }
 
   ngOnInit(): void {
+    this.getUserData();
+    }
+    getUserData() {
+      this.api.getData().subscribe((res:any[]) => {
+        this.userData = res;
+        console.log(res);
+      });
+    }
+    deleteUserData(data : any){
+      this.api.deleteData(data.id)
+      .subscribe(res=>{
+        alert("Employee Deleted");
+        this.getUserData();
+      })
+    }
+    
   }
+  
 
-}
+
---
+++ src/app/dashboard/admin-dashboard/admin.model.ts
@@ -0,0 +1,6 @@
+export class dataModel{
+    id: number = 0;
+    fullName : string ='';
+    email : string ='';
+    mobile : string ='';
+}
